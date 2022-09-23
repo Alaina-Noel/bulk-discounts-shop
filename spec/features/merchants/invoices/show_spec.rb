c678450b333fc46303cde0135db4fa113c8ea030
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-23 17:04:06 -0600

    Created block for new tests

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -2,31 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Index Show Page' do
 
-  # test data collapsed here
   let!(:jewlery_city) { Merchant.create!(name: "Jewlery City Merchant")}
   let!(:carly_silo) { Merchant.create!(name: "Carly Simon's Candy Silo")}
-
   let!(:gold_earrings) { jewlery_city.items.create!(name: "Gold Earrings", description: "14k Gold 12' Hoops", unit_price: 12000) }
   let!(:silver_necklace) { jewlery_city.items.create!(name: "Silver Necklace", description: "An everyday wearable silver necklace", unit_price: 220000) }
   let!(:studded_bracelet) { jewlery_city.items.create!(name: "Gold Studded Bracelet", description: "A bracet to make others jealous", unit_price: 2900) } #no one is buying the studded bracelet so it should not appear in the tests
   let!(:licorice) { carly_silo.items.create!(name: "Licorice Funnels", description: "Licorice Balls", unit_price: 1200, enabled: true) }
-
   let!(:alaina) { Customer.create!(first_name: "Alaina", last_name: "Kneiling")}
-
   let!(:alaina_invoice1) { alaina.invoices.create!(status: "completed")}
   let!(:alaina_invoice2) { alaina.invoices.create!(status: "in_progress")}
-
-# alaina_invoice1 should have, from Jewelry, gold earrings and silver necklace, NOT studded bracelet. should also have licorice id, but that should not be shown for this merchant
   let!(:alainainvoice1_itemgold_earrings) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: gold_earrings.id, quantity: 4, unit_price: 1300, status:"packaged" )}
   let!(:alainainvoice9_itemgold_earrings) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: gold_earrings.id, quantity: 6, unit_price: 1111, status:"packaged" )}
-
   let!(:alainainvoice1_itemsilver_necklace) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: silver_necklace.id, quantity: 7, unit_price: 1500, status:"packaged" )}
-
   let!(:alainainvoice1_itemsilver_necklace) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: silver_necklace.id, quantity: 4, unit_price: 1300, status:"packaged" )}
-
   let!(:alainainvoice1_itemglicorice) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: licorice.id, quantity: 4, unit_price: 1300, status:"packaged" )}
-
-  #this invoice contains an item belonging to this merchant, but no info should be should on invoice1 show page
   let!(:alainainvoice2_itemstudded_bracelet) { InvoiceItem.create!(invoice_id: alaina_invoice2.id, item_id: studded_bracelet.id, quantity: 40, unit_price: 1500, status:"shipped" )}
   
   describe 'when I visit a merchant invoice show page' do
@@ -101,8 +90,12 @@ RSpec.describe 'Merchant Index Show Page' do
           expect(page).to have_content("Total Revenue From This Invoice: $#{sprintf("%.2f",alaina_invoice1.calculate_invoice_revenue/100.to_f)}")
         end
       end
+    end
 
-      it 'Then I see two total revenue for my merchant from this invoice one with the discount and one without the discount' do
+
+    describe 'Invoice Disount Related Displays' do
+
+      xit 'Then I see two total revenue for my merchant from this invoice one with the discount and one without the discount' do
         visit merchant_invoice_path(jewlery_city, alaina_invoice1)
 
         within("#total_invoice_revenue") do
@@ -110,10 +103,11 @@ RSpec.describe 'Merchant Index Show Page' do
           expect(page).to have_content("Total Revenue From This Invoice Discount Applied: $#{sprintf("%.2f",alaina_invoice1.calculate_discounted_invoice_revenue/100.to_f)}")
         end
       end
-
-
-
     end
+
+
+
+   
   end
 end
 
