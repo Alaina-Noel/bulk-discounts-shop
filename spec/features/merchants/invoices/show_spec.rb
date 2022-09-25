8ef115ea342abc7ef2185665211a34f12131db4b
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-09-24 19:56:57 -0600

    tests added to see a link to discount show page

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -111,6 +111,41 @@ RSpec.describe 'Merchant Index Show Page' do
         end
       end
 
+      describe 'When I visit my merchant invoice show page' do
+
+        let!(:jewlery_city) { Merchant.create!(name: "Jewlery City Merchant")}
+  
+        let!(:jcity_discount1) {jewlery_city.bulk_discounts.create!(percentage_discount: 20, quantity_threshold: 10)}
+        let!(:jcity_discount2) {jewlery_city.bulk_discounts.create!(percentage_discount: 30, quantity_threshold: 15)}
+        
+        let!(:gold_earrings) { jewlery_city.items.create!(name: "Gold Earrings", description: "14k Gold 12' Hoops", unit_price: 12000) }
+        let!(:silver_necklace) { jewlery_city.items.create!(name: "Silver Necklace", description: "An everyday wearable silver necklace", unit_price: 22000) }
+        let!(:studded_bracelet) { jewlery_city.items.create!(name: "Studded Bracelet", description: "A dainty studded bracelet", unit_price: 1100) }
+  
+        let!(:alaina) { Customer.create!(first_name: "Alaina", last_name: "Kneiling")}
+        let!(:alaina_invoice1) { alaina.invoices.create!(status: "completed")}
+  
+        let!(:alainainvoice1_itemgold_earrings) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: gold_earrings.id, quantity: 12, unit_price: 1300, status:"packaged" )}
+        let!(:alainainvoice1_itemsilver_necklace) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: silver_necklace.id, quantity: 15, unit_price: 1300, status:"packaged" )}
+        let!(:alainainvoice1_itemstudded_bracelet) { InvoiceItem.create!(invoice_id: alaina_invoice1.id, item_id: studded_bracelet.id, quantity: 1, unit_price: 1100, status:"packaged" )}
+
+        it 'Next to each invoice item I see a link to the show page for the bulk discount that was applied (if any)' do
+          visit merchant_invoice_path(jewlery_city, alaina_invoice1)
+
+          within("#item_#{gold_earrings.id}") do
+            expect(page).to have_content("Discount Details")
+          end
+
+          within("#item_#{silver_necklace.id}") do
+            expect(page).to have_content("Discount Details")
+          end
+
+          within("#item_#{studded_bracelet.id}") do
+            expect(page).to_not have_content("Discount Details")
+          end
+        end
+      end
+
 
 
     end
